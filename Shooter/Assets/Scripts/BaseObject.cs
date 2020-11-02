b60deeeabeeba802dba4ef6a9bffd91a622e33f2
tretyigo <igor.tret@gmail.com>
2020-11-02 05:30:11 +0300

    FlashLight

---
+++ Shooter/Assets/Scripts/BaseObject.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+/// <summary>
+/// Базовый класс, кторый кэширует ссылки
+/// </summary>
+
+public abstract class BaseObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform goTransform;
+    private GameObject goInstance;
+    private string GOname;
+
+    private Rigidbody goRigidbody;
+
+    private Material goMaterial;
+    private Color goColor;
+    private Animator goAnimator;
+    private bool isVisible;
+
+    protected Transform GoTransform { get => goTransform; set => goTransform = value; }
+    protected GameObject GoInstance { get => goInstance; set => goInstance = value; }
+    protected string GoName { get => GOname; set => GOname = value; }
+    protected Rigidbody GoRigidbody {get => goRigidbody; set => goRigidbody = value; }
+    protected Material GoMaterial { get => goMaterial; set => goMaterial = value; }
+    protected Color GoColor { get => goColor; set => goColor = value; }
+    protected Animator GoAnimator { get => goAnimator; set => goAnimator = value; }
+    protected bool IsVisible { get => isVisible; set => isVisible = value; }
+
+    protected virtual void Awake()
+    {
+        GoTransform = transform;
+        GoName = name;
+        GoInstance = gameObject;
+
+
+        if(GetComponent<Animator>())
+        {
+            GoAnimator = GetComponent<Animator>();
+        }
+        else
+        {
+            Debug.Log("NO ANIMATOR" + GoName); 
+        }
+        if (GetComponent<Rigidbody>())
+        {
+            GoRigidbody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.Log("NO RGIDBODY" + GoName);
+        }
+
+        if (GetComponent <Renderer>())
+        {
+            GoMaterial = GetComponent<Renderer>().material;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Shooter/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class FlashLight : BaseObject
+{
+
+    [SerializeField] private Light flashlightLight;
+
+    private float timeout = 10;
+    private float currTime = 0;
+    private float currReloadTime = 0;
+
+    private float maxBattery = 10;
+    private float currBattery = 10;
+
+
+    private KeyCode control = KeyCode.F;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        flashlightLight = GetComponentInChildren<Light>();
+    }
+
+    private void ActiveFlashLight(bool val)
+    {
+        flashlightLight.enabled = val;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyUp(control))
+        {
+
+            if (!flashlightLight.enabled)
+            {
+                Debug.Log("Включение");
+                Debug.Log("Заряд батареи:" + currBattery);
+                ActiveFlashLight(true);
+            }
+            else
+            {
+                Debug.Log("Выключение");
+                Debug.Log("Заряд батареи:" + currBattery);
+                ActiveFlashLight(false);
+            }
+        }
+        
+        if (flashlightLight.enabled)
+        {
+            currBattery -= Time.deltaTime;
+            if (currBattery <= 0)
+            {
+                ActiveFlashLight(false);
+
+            }
+        }
+
+        if (!flashlightLight.enabled && currBattery <= 10)
+        {
+            currBattery += Time.deltaTime;
+        }
+    }
+}
